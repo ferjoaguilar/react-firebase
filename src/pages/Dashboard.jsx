5e75e9e4f54537fce2aa7ccc852bf7d9162cdd2a
Fernando Aguilar <feraguilar6985@gmail.com>
2023-09-07 17:56:17 -0600

    Protected routes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
-
-
+import React, { useEffect } from 'react'
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import app from '../utils/firebase';
+import { useNavigate } from "react-router-dom"
 
 const Dashboard = () => {
+
+  const auth = getAuth(app)
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+   onAuthStateChanged(auth, (user) => {
+    if(user){
+      console.log("Bienvenido a la ruta protegida");
+    }else {
+      console.log("Ve devuelta a hacer login");
+      navigate('/login')
+    }
+   })  
+  }, [])
+  
+
+
   return (
     <h1 className='text-white'>DASHBOARD RUTA PROTEGIDA</h1>
   )
